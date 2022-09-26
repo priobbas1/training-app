57d6a9422e948374dab88ee900426fafd2913451
yojanny <yojannyreynoso@gmail.com>
2022-09-26 13:08:35 +0200

    actualizando datos

--- my-training-app/src/components/Login.js
+++ my-training-app/src/components/Login.js
@@ -1,72 +1,110 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import { loginAccount } from "../http/user-api";
-import '../components/hojas-stilos/Login.css';
+import { useEffect, useState } from "react";
+import { AuthProvider } from "../shared/context/authContext";
+
+import "../components/hojas-stilos/Login.css";
 import { Link } from "react-router-dom";
 
-export const Login = () => {
-  const initialState = { email: "", password: "" };
-  const [loginForm, setLoginForm] = useState(initialState);
-  const [showPassword, setShowPassword] = useState(false);
+const schema = yup
+  .object({
+    email: yup.string().email().required(),
+    password: yup.string().min(3).max(30).required(),
+  })
+  .required();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+function Login() {
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [isSubmitSuccessful, setIsSubmitSuccessful] = useState(false);
+
+  const onSubmit = async (data) => {
     try {
-      const res = await loginAccount(loginForm);
+      const res = await loginAccount(data);
       if (res.status === 200) {
-        console.log(res.data[1]);
+        console.log(res.data);
+        localStorage.setItem("token", res.data[1].accesToken);
+        //updateToken();
+        AuthProvider();
+        setIsSubmitSuccessful(true);
+      } else if (res.status === "400") {
+        console.log(res);
+        setIsSubmitSuccessful(false);
+      } else if (res.status === "404") {
+        console.log(res);
+        setIsSubmitSuccessful(false);
+      } else if (res.status === "401") {
+        console.log(res);
+        setIsSubmitSuccessful(false);
+      } else if (res.status === "500") {
+        console.log(res);
+        setIsSubmitSuccessful(false);
       }
     } catch (e) {
-      console.log(e);
+      console.error(e);
     }
   };
-  const handleChange = (e, field) => {
-    const modifiedData = { ...loginForm, [field]: e.target.value };
-    setLoginForm(modifiedData);
-  };
+
+  useEffect(() => {
+    if (!isSubmitSuccessful) return;
+
+    reset({ email: "", password: "" });
+  }, [isSubmitSuccessful, reset]);
 
   return (
-    <section>
- <form onSubmit={handleSubmit} className="login">
-        
-        <label htmlFor="user">User</label>
+    <>
+      <section className="login">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="email">email</label>
+          <input
+            {...register("email")}
+            autoComplete="off"
+            placeholder="example@mail.com"
+          ></input>
+          <p>{errors.email?.message}</p>
 
-        <input
-          value={loginForm.email}
-          onChange={(e) => {
-            handleChange(e, "email");
-          }}
-          name="email"
-          id="email"
-        ></input>
-      
+          <label htmlFor="password">password</label>
+          <input
+            {...register("password")}
+            type={showPassword ? "text" : "password"}
+            autoComplete="off"
+            placeholder="************"
+          ></input>
+          <p>{errors.password?.message}</p>
 
-      
-        <label htmlFor="password">Password</label>
-        <input
-          value={loginForm.password}
-          onChange={(e) => {
-            handleChange(e, "password");
-          }}
-          name="password"
-          id="password"
-          type={showPassword ? "text" : "password"}
-        ></input>
-      
+          <input type="submit" value="login"></input>
+          <input
+            type="button"
+            onClick={() => {
+              reset({ email: "", password: "" }, { keepErrors: true });
+            }}
+            value="clear"
+          />
+        </form>
+        <button
+          onClick={() => setShowPassword(showPassword ? false : true)}
+          className="password"
+        >
+          See password
+        </button>
+      </section>
 
-      <input type="submit" className="enviar"></input>
-      <button onClick={() => setLoginForm(initialState)} className="clear">
-        Clear
-      </button>
-      <button
-        onClick={() => setShowPassword(showPassword ? false : true)}
-        className="password"
-      >
-        See password
-      </button>
-    </form>
-  
-<span className="atras"><Link to={"/"}>Go to Home</Link></span>
-    </section>
-     
+      <span>
+        <Link to={"/"}>Go to Home</Link>
+      </span>
+    </>
   );
-};
+}
+
+export default Login;
